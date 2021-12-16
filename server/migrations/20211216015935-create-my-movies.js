50c787225e00dd667808ff38d64b83280c610e7e
KRLEESEUNGJAE <karmaup0711@gmail.com>
2021-12-16 17:35:49 +0900

    Feat: Add models and migrations !BREAKING CHANGE: Add database structure and relations

---
+++ server/migrations/20211216015935-create-my-movies.js
@@ -0,0 +1,45 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("myMovies", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      movieId: {
+        type: Sequelize.INTEGER,
+      },
+      movieName: {
+        type: Sequelize.STRING(50),
+      },
+      moviePoster: {
+        type: Sequelize.STRING,
+      },
+      movieGenre: {
+        type: Sequelize.INTEGER,
+      },
+      movieStars: {
+        type: Sequelize.INTEGER,
+      },
+      movieRunningtime: {
+        type: Sequelize.INTEGER,
+      },
+      movieDesc: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("myMovies");
+  },
+};
